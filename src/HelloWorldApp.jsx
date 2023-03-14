8c4b0eec9051929d2a5bf47a56facc816a502c0e
Jean Gonzales <expretapador3@gmail.com>
2023-03-14 12:04:57 -0500

    primer componente en un archivo propio

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,11 @@
+// export function App() { //componente inicial pero es de mala practica que este en este archivo main 
+//     return (<h1>Hola mundo!!!</h1>);
+// }
+
+//rafc shortcut
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>HelloWorldApp</h1>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react'; //importamos react 
 import  ReactDOM  from 'react-dom/client'; //herramienta para renderizar react como es web usamos DOM
+import { HelloWorldApp } from './HelloWorldApp';
 
-function App() { //componente inicial pero es de mala practica que este en este archivo main 
-    return (<h1>Hola mundo!!!</h1>);
-}
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render( //forma de renderizar nuestro componente
     <React.StrictMode>
-        <App />
+        <HelloWorldApp />
     </React.StrictMode>
 );
