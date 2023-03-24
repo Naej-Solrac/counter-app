b70c200f7715b65113ae3bf2f0efd4b743778a23
Jean Gonzales <expretapador3@gmail.com>
2023-03-24 12:39:01 -0500

    Tarea - componente CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 
 import React from 'react'; //importamos react 
 import  ReactDOM  from 'react-dom/client'; //herramienta para renderizar react como es web usamos DOM
+import { CounterApp } from './CounterApp';
 import { HelloWorldApp } from './HelloWorldApp';
 import { Naej } from './Naej';
 import './styles.css';
 
-
 ReactDOM.createRoot(document.getElementById('root')).render( //forma de renderizar nuestro componente
     <React.StrictMode>
-        <Naej/>
+        <CounterApp value = {991014002}/>
     </React.StrictMode>
 );
 
