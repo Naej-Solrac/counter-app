b94d8e9209e2ae09f75a148785747f745c82a1fd
Jean Gonzales <expretapador3@gmail.com>
2023-03-27 10:56:20 -0500

    evento onClick en general

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,13 +1,26 @@
 import PropTypes from 'prop-types'; 
 import React from 'react'
 
+
 export const CounterApp = ({value}) => {
+
+  const handleAdd = () => {
+    // console.log(event);
+    console.log(value);
+    value = 1000;
+  };
+
   return (
     <>
         <h1>CounterApp</h1>
         <h2> { value } </h2>
+
+        <button onClick={ handleAdd }>
+          +1
+        </button>
     </>
   );
+
 };
 
 CounterApp.prototype = {
